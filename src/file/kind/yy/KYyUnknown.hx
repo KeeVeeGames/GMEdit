7b444f9207a3257c9e03686798cf9458ad18121c
MusNik <admin@keevee.games>
2026-07-09 13:52:05 +0400

    Particle system and sequence assets are now recognized
    (as they always deserved)

--- src/file/kind/yy/KYyUnknown.hx
+++ src/file/kind/yy/KYyUnknown.hx
@@ -68,11 +68,12 @@ class KYyUnknown extends FileKind {
 		//
 		if (dir != null) {
 			var makeEl = true;
-			var kind = resType.substring(2).toLowerCase();
+			var kind = YyTools.kindOf23(resType);
 			switch (resType) {
 				case "GMSprite", "GMTileSet", "GMSound", "GMPath",
 					"GMScript", "GMShader", "GMFont", "GMTimeline",
-					"GMObject", "GMRoom", "GMAnimCurve"
+					"GMObject", "GMRoom", "GMAnimCurve", "GMSequence",
+					"GMParticleSystem"
 				: { // assets
 					var aceKind = "asset." + kind;
 					GmlAPI.gmlKind.set(name, aceKind);
